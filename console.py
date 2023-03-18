f8b72882b85214f5efc7033dcea401702e3c2eb6
josemls <jose_9513@hotmail.com>
2023-03-18 10:25:32 -0400

    console

--- console.py
+++ console.py
@@ -2,8 +2,17 @@
 """
 Creating the command interpreter console
 """
-
+import models
 import cmd
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models import storage
+
+
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -24,6 +33,18 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing on empty line."""
         pass
+
+    def do_create(self, line):
+        if len(line) == 0:
+            print("** class name missing **")
+        else:
+            if line not in CLASSES:
+                print("** class doesn't exist **")
+            else:
+                str = line + "()"
+                base_inst = eval(str)
+                base_inst.save()
+                print(base_inst.id)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
